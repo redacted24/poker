f6b82c408932ef4ad8ac954e26a3508221702484
redacted24 <huangq4@outlook.com>
2024-04-08 10:12:43 -0400

    adding assertions for the better bot

--- bots.py
+++ bots.py
@@ -3,8 +3,10 @@ from game import *
 class Better(Player):
     '''A bot that always bets 99$, or all of his balance if it is less than 99$.'''
     def play(self):
-        if self.balance <= 99:      # Check if balance is less than 99.
+        if self.balance >= 99:      # Check if balance is less than 99.
             self.bet(99)            # Bet 99 if it is more or equal to 99.
+        elif self.balance == 0:
+            raise ValueError('you broke ahh')       # If bot doesn't have anymore money, they lose.
         else:
             self.bet(self.balance)  # Otherwise, bet the remaining balance of the bot.
 
@@ -23,11 +25,21 @@ if __name__ == "__main__":
     scary_cat = ScaryCat('Cat', table)
     better = Better('Better', table)
 
-    table.pre_flop()
+    assert scary_cat.name == 'Cat'
+
+    table.pre_flop()        # Pre-flop
     for i in range(2):
         scary_cat.receive(table.deck.draw())
+        table.deck.burn
         better.receive(table.deck.draw())
+    better.play()           # Better bot plays (bets 99)
+    better.play()           # Better bot plays (bets 99)
 
-    assert scary_cat.name == 'Cat'
+    assert better.balance == 1000-99*2
+    assert table.pot == 99*2
+
+    for i in range(8):
+        better.play()
+    
 
     print('All tests passed')
