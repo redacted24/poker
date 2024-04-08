7ad8744bd52ca9247f9c5b5608688cfc6780859f
redacted24 <huangq4@outlook.com>
2024-04-07 21:41:28 -0400

    added Joker bot

--- bots.py
+++ bots.py
@@ -4,6 +4,9 @@ class ScaryCat(Player):
     '''A bot that folds if the opponent bets.'''
     def test(self):
         return self.name
+
+class Joker(Player):
+    '''A bot that only does random actions. Can bet a random multiplier of the small blind'''
     
 
 if __name__ == "__main__":
