70f4af09d121328c514afec807c6e2209b12dfe1
Crazydodo123 <peizheg@gmail.com>
2024-04-09 15:44:48 -0400

    add documentation for server

--- server/poker/__init__.py
+++ server/poker/__init__.py
@@ -1,19 +1,11 @@
-import os
-
 from flask import Flask
 
-
 def create_app():
-    app = Flask(__name__)
-    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app = Flask(__name__)                               # create and configure the server
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'            # sets secret key for signing session cookies (will change later)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-    
+    # import poker logic into the server
     from . import poker
     app.register_blueprint(poker.bp)
-        
 
     return app
--- server/poker/poker.py
+++ server/poker/poker.py
@@ -1,8 +1,12 @@
 from flask import ( Blueprint, session )
 
+# define /api/poker to be the point of contact for backend logic
 bp = Blueprint('poker', __name__, url_prefix='/api/poker')
 
-@bp.route('/hi')
-def hi():
-  session['test'] = session.get('test', 0) + 1
-  return str(session['test'])
+
+@bp.route('/count')
+def count():
+  '''Test function for server/site communication and session variables'''
+
+  session['count'] = session.get('count', 0) + 1          # increment session variable 'count' by 1
+  return str(session['count'])
