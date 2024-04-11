003f875a3ed9c2aa1f9e570027aa11ae964b7ae7
redacted24 <huangq4@outlook.com>
2024-04-11 15:38:15 -0400

    fix game.py changes testcases

--- tests/game_tests.py
+++ tests/game_tests.py
@@ -8,32 +8,35 @@ class TestTableMethods(unittest.TestCase):
     def setUp(self):            # Run this for every unit test
         self.deck = Deck()
         self.table = Table(self.deck)
-        self.p1 = Player('Player1', self.table)
+        self.p1 = Player('Player1', False, self.table)
+        self.table.pre_flop()
 
     def tearDown(self):
         self.table.end()
 
     def test_boardSize_preflop(self):
-        self.table.pre_flop()
-        self.assertEqual(len(self.table.board),3,'Incorrect number of cards on board')
+        self.assertEqual(len(self.table.board.display()),3,'Incorrect number of cards on board')
 
     def test_boardSize_flop(self):
-        self.table.pre_flop()
-        self.table.flop()
-        self.assertEqual(len(self.table.board), 3, 'Incorrect number of cards on board')
+        self.p1.call()
+        self.table.play()
+        self.assertEqual(len(self.table.board.display()), 3, 'Incorrect number of cards on board')
 
     def test_boardSize_turn(self):
-        self.table.pre_flop()
-        self.table.flop()
-        self.table.turn()
-        self.assertEqual(len(self.table.board), 4, 'Incorrect number of cards on board')
+        self.p1.call()
+        self.table.play()
+        self.p1.call()
+        self.table.play()
+        self.assertEqual(len(self.table.board.display()), 4, 'Incorrect number of cards on board')
 
     def test_boardSize_river(self):
-        self.table.pre_flop()
-        self.table.flop()
-        self.table.turn()
-        self.table.river()
-        self.assertEqual(len(self.table.board), 5, 'Incorrect number of cards on board')
+        self.p1.call()
+        self.table.play()
+        self.p1.call()
+        self.table.play()
+        self.p1.call()
+        self.table.play()
+        self.assertEqual(len(self.table.board.display()), 5, 'Incorrect number of cards on board')
     
     def test_playerBetIncreasesPot(self):
         self.p1.bet(100)
@@ -41,100 +44,28 @@ class TestTableMethods(unittest.TestCase):
 
     def test_addCard(self):
         self.table.add_card()
-        self.assertEqual(len(self.table.board), 1, 'adding card does not add a card to the board correctly')
-    
-    def test_roundStats(self): 
-        self.p1.bet(100)
-        self.p1.bet(100)
-        self.p1.check()
-        self.p1.call()
-        self.p1.fold()
-        self.p1.all_in()
-        output =    {
-            'bet': 2,
-            'raise': 0,
-            'call': 1,
-            'check': 1,
-            'all-in': 1,
-            'fold': 1
-        }
-        self.assertDictEqual(self.table.round_stats, output, 'Incoherent round stats')
-    
-    def test_roundStatsReset(self):
-        self.p1.bet(100)
-        self.p1.bet(100)
-        self.p1.check()
-        self.p1.call()
-        self.p1.fold()
-        self.table.reset()
-        self.p1.bet(100)
-        output =    {
-            'bet': 1,
-            'raise': 0,
-            'call': 0,
-            'check': 0,
-            'all-in': 0,
-            'fold': 0
-        }
-        self.assertDictEqual(output, self.table.round_stats, 'Round stats reset does not work.')
-
-    def test_gameStats(self):
-        self.p1.bet(100)
-        self.p1.bet(100)
-        self.p1.all_in()
-        self.table.end()                            # Player leave table
-        self.p1 = Player('Player1', self.table)     # Players rejoin table. Game stats should have reset
-        self.p1.bet(100)
-        output =    {
-            'bet': 1,
-            'raise': 0,
-            'call': 0,
-            'check': 0,
-            'all-in': 0,
-            'fold': 0
-        }
-        self.assertDictEqual(self.table.round_stats, output, 'Incoherent round stats')
+        self.assertEqual(len(self.table.board.display()), 1, 'adding card does not add a card to the board correctly')
     
     def test_lastMoveBet(self):
         self.p1.bet(100)
-        self.assertEqual(self.table.last_move, [self.p1,'bet'], 'Incoherent last move')
+        self.assertEqual(self.table.last_move, [self.p1.name,'bet'], 'Incoherent last move')
 
     def test_lastMoveFold(self):
         self.p1.fold()
-        self.assertEqual(self.table.last_move, [self.p1,'fold'], 'Incoherent last move')
-
-    def test_lastMoveAllIn(self):
-        self.p1.all_in()
-        self.assertEqual(self.table.last_move, [self.p1,'all-in'], 'Incoherent last move')
+        self.assertEqual(self.table.last_move, [self.p1.name,'fold'], 'Incoherent last move')
 
     def test_lastMoveCheck(self):
         self.p1.check()
-        self.assertEqual(self.table.last_move, [self.p1,'check'], 'Incoherent last move')
+        self.assertEqual(self.table.last_move, [self.p1.name,'check'], 'Incoherent last move')
 
     def test_lastMoveCall(self):
         self.p1.call()
-        self.assertEqual(self.table.last_move, [self.p1,'call'], 'Incoherent last move')
+        self.assertEqual(self.table.last_move, [self.p1.name,'call'], 'Incoherent last move')
     
     def test_playersOnTable(self):
-        self.p2 = Player('p2', self.table)
-        self.p3 = Player('p3', self.table)
+        self.p2 = Player('p2', False, self.table)
+        self.p3 = Player('p3', False, self.table)
         self.assertEqual([self.p1, self.p2, self.p3], self.table.players, 'Incorrect players on table')
-    
-    def test_gameStatePreflop(self):
-        self.table.pre_flop()
-        self.assertEqual(self.table.state, 0, 'Incoherent game state')
-    
-    def test_gameStateFlop(self):
-        self.table.flop()
-        self.assertEqual(self.table.state, 1, 'Incoherent game state')
-    
-    def test_gameStateTurn(self):
-        self.table.turn()
-        self.assertEqual(self.table.state, 2, 'Incoherent game state')
-    
-    def test_gameStateRiver(self):
-        self.table.river()
-        self.assertEqual(self.table.state, 3, 'Incoherent game state')
 
 
 # ---
@@ -142,7 +73,7 @@ class TestPlayerMethods(unittest.TestCase):
     def setUp(self):
         self.deck = Deck()
         self.table = Table(self.deck)
-        self.p1 = Player('Player1', self.table)
+        self.p1 = Player('Player1', False, self.table)
 
     def tearDown(self):
         self.table.end()
