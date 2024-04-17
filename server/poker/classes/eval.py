a3dca6bb425083c735933481f8a858ee3fa33b5b
Crazydodo <peizheg@gmail.com>
2024-04-17 10:14:27 -0400

    calculate ppot2 and npot2 for eval function

--- server/poker/classes/eval.py
+++ server/poker/classes/eval.py
@@ -1,4 +1,5 @@
 try:
+    from itertools import combinations
     from poker.classes.cards import Deck
     from poker.classes.game import Player
 except:
@@ -60,44 +61,65 @@ class eval():
         return (win + 0.5 * tie) / sum([win, tie, loss])
 
     def potential_hand_strength(self):
+        hand_potentials = [[0] * 3 for _ in range(3)]
+        
         p1 = Player('player', True)
         p2 = Player('opponent', True)
 
         p1.receive(self.hand)
 
+        p1_rank_5 = p1.handEval(self.board_cards)
+
         d = Deck()
-        computed_ranks = {}
+        computed_p1_ranks = {}
+        computed_p2_ranks = {}
 
         filtered_deck = self.remove_cards(d, self.hand + self.board_cards)
-        
-        win = tie = loss = 0
-        for i, c1 in enumerate(filtered_deck):
-            for c2 in filtered_deck[i+1:]:
-                predicted_board_cards = self.board_cards + [c1, c2]
-                p1_rank = p1.handEval(predicted_board_cards)
 
-                new_deck = self.remove_cards(filtered_deck, [c1, c2])
+        for c1, c2 in list(combinations(filtered_deck, 2)):
+            p2.clear_hand()
+            p2.receive([c1, c2])
 
-                for k, c3 in enumerate(new_deck):
-                    for c4 in new_deck[k+1:]:
-                        p2.clear_hand()
-                        p2.receive([c3, c4])
-                        str_cards = ''.join(sorted([c.shortName for c in [c1, c2, c3, c4]]))
-                        if str_cards in computed_ranks:
-                            p2_rank = computed_ranks[str_cards]
-                        else:
-                            p2_rank = p2.handEval(predicted_board_cards)
-                            computed_ranks[str_cards] = p2_rank
+            p2_rank_5 = p2.handEval(self.board_cards)
 
-                        if p1_rank > p2_rank:
-                            win += 1
-                        elif p1_rank == p2_rank:
-                            tie += 1
-                        else:
-                            loss += 1
-        
-        print(win, tie, loss)
-        return (win + 0.5 * tie) / sum([win, tie, loss])
+            if p1_rank_5 > p2_rank_5:
+                i = 0           # We are ahaed
+            elif p1_rank_5 == p2_rank_5:
+                i = 1           # We are tied
+            else:
+                i = 2           # We are behind
+
+            new_filtered_deck = self.remove_cards(filtered_deck, [c1, c2])
+
+            for c3, c4 in list(combinations(new_filtered_deck, 2)):
+                predicted_board_cards = self.board_cards + [c3, c4]
+                str_cards_p1 = ''.join(sorted([c.shortName for c in [c3, c4]]))
+                str_cards_p2 = ''.join(sorted([c.shortName for c in [c1, c2, c3, c4]]))
+
+                if str_cards_p1 in computed_p1_ranks:
+                    p1_rank_7 = computed_p1_ranks[str_cards_p1]
+                else:
+                    p1_rank_7 = p1.handEval(predicted_board_cards)
+                    computed_p1_ranks[str_cards_p1] = p1_rank_7
+
+                if str_cards_p2 in computed_p2_ranks:
+                    p2_rank_7 = computed_p2_ranks[str_cards_p2]
+                else:
+                    p2_rank_7 = p2.handEval(predicted_board_cards)
+                    computed_p2_ranks[str_cards_p2] = p2_rank_7
+            
+                if p1_rank_7 > p2_rank_7:
+                    hand_potentials[i][0] += 1
+                elif p1_rank_7 == p2_rank_7:
+                    hand_potentials[i][1] += 1
+                else:
+                    hand_potentials[i][2] += 1
+
+        ppot2 = (hand_potentials[2][0] + hand_potentials[2][1] / 2 + hand_potentials[1][0]) / (sum(hand_potentials[2]) + sum(hand_potentials[1]) / 2)
+        npot2 = (hand_potentials[0][2] + hand_potentials[0][1] / 2 + hand_potentials[1][2]) / (sum(hand_potentials[0]) + sum(hand_potentials[1]) / 2)
+
+        return ppot2, npot2
+
     
 
 d = Deck()
