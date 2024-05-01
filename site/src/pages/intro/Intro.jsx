8415a7dfb023b9bb777ec711a30110c3f545faf9
redacted24 <huangq4@outlook.com>
2024-05-01 10:56:05 -0400

    add lil castle animation to campaign

--- site/src/pages/intro/Intro.jsx
+++ site/src/pages/intro/Intro.jsx
@@ -6,7 +6,12 @@ const Intro = () => {
     <div id="intro-container">
       <h1 id='title'>The Poker Playground</h1>
       <div id='link-buttons'>
-        <Link className="react-link-clickable" to='/playground'><button id="campaign" className='link-button'>Campaign</button></Link>
+        <Link className="react-link-clickable" to='/playground'>
+          <button id="campaign" className='link-button'>
+            Campaign
+          </button>
+          <img src="../../src/assets/ui/campaign-castle.svg" alt="doesnt work" id="campaign-image-1"></img>
+        </Link>
         <Link className="react-link-clickable" to='/playground'><button id="quickstart" className='link-button'>Quick Start</button></Link>
         <Link className="react-link-clickable" to='/host'><button id="create-table" className='link-button'>Create a Table</button></Link>
       </div>
