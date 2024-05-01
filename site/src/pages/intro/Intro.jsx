9fc8939e6951f3d6a7373f1149647d83633684d1
redacted24 <huangq4@outlook.com>
2024-05-01 18:27:40 -0400

    finish campaign menu option effect

--- site/src/pages/intro/Intro.jsx
+++ site/src/pages/intro/Intro.jsx
@@ -1,45 +1,36 @@
 import { Link } from 'react-router-dom'
-import { useEffect } from 'react'
 import './intro.css'
 
 const Intro = () => {
-  // useEffect(() => {
-  //   // Used to disable animation load on page load
-  //   const DisableLoadAnimation = () => {
-  //     let campaign = document.getElementById('campaign')
-  //     let quickstart = document.getElementById('quickstart')
-  //     let multiplayer = document.getElementById('multiplayer')
-      
-  //     const addAnimCampaign = () => {
-  //       console.log('campaign mouse over')
-  //       document.getElementById('campaign-image-1').classList.add('back-animation')
-  //       document.getElementById('campaign-image-2').classList.add('back-animation')
-  //       campaign.removeEventListener('mouseover', addAnimCampaign)
-  //     }
-  //     campaign.addEventListener('mouseover', addAnimCampaign)
-  //   }
-  //   // DisableLoadAnimation()
-  // })
-
   return (
     <div id="intro-container">
       <h1 id='title'>The Poker Playground</h1>
       <div id='link-buttons'>
         <Link className="react-link-clickable" to='/playground'>
           <button id="campaign" className='link-button'>
-            Campaign
+            <div id="campaign-text">Campaign</div>
           </button>
           <img src="../../src/assets/ui/campaign-castle.svg" alt="doesnt work" id="campaign-image-1"></img>
-          <img src="../../src/assets/ui/tree1.svg" alt="doesnt work" id="campaign-image-2"></img>
+          <img src="../../src/assets/ui/shield.svg" alt="doesnt work" id="campaign-image-2"></img>
+          <img src="../../src/assets/ui/swords.svg" alt="doesnt work" id="campaign-image-3"></img>
+          <img src="../../src/assets/ui/heart_filled.svg" alt="doesnt work" id="campaign-image-6"></img>
+          <img src="../../src/assets/ui/heart.svg" alt="doesnt work" id="campaign-image-4"></img>
+          <img src="../../src/assets/ui/heart.svg" alt="doesnt work" id="campaign-image-5"></img>
+          <img src="../../src/assets/ui/sun.svg" alt="doesnt work" id="campaign-image-7"></img>
+          <img src="../../src/assets/ui/cloud.svg" alt="doesnt work" id="campaign-image-8"></img>        
+          <img src="../../src/assets/ui/cloud.svg" alt="doesnt work" id="campaign-image-9"></img>        
+          <img src="../../src/assets/ui/line.svg" alt="doesnt work" id="campaign-image-10"></img>        
+          <img src="../../src/assets/ui/bird.svg" alt="doesnt work" id="campaign-image-11"></img>        
         </Link>
         <Link className="react-link-clickable" to='/playground'>
           <button id="quickstart" className='link-button'>
-            Quick Start
-          </button></Link>
+            <div id="quickstart-text">Quick Start</div>
+          </button>
           <img src="" alt="placeholder" id="quickstart-image-1"></img>
+        </Link>
         <Link className="react-link-clickable" to='/host'>
           <button id="multiplayer" className='link-button'>
-            Create a Table
+            <div id="multiplayer-text">Create a Table</div>
           </button>
           <img src="" alt="wtf" id="multiplayer-image-1"></img>
         </Link>
