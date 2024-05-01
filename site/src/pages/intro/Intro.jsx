3c282015a1e2f4dd630840aced07c9ec82b7463a
redacted24 <huangq4@outlook.com>
2024-04-30 22:57:03 -0400

    update lobby

--- site/src/pages/intro/Intro.jsx
+++ site/src/pages/intro/Intro.jsx
@@ -3,14 +3,14 @@ import './intro.css'
 
 const Intro = () => {
   return (
-    <>
+    <div id="intro-container">
       <h1 id='title'>The Poker Playground</h1>
       <div id='link-buttons'>
-        <Link to='/playground'><button className='link-button'>Campaign</button></Link>
-        <Link to='/playground'><button className='link-button'>Quick Start</button></Link>
-        <Link to='/host'><button className='link-button'>Create a Table</button></Link>
+        <Link className="react-link-clickable" to='/playground'><button id="campaign" className='link-button'>Campaign</button></Link>
+        <Link className="react-link-clickable" to='/playground'><button id="quickstart" className='link-button'>Quick Start</button></Link>
+        <Link className="react-link-clickable" to='/host'><button id="create-table" className='link-button'>Create a Table</button></Link>
       </div>
-    </>
+    </div>
   )
 }
 
