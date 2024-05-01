d3f465fd79ce148d9dd6e56ded23c9548f09a5a2
redacted24 <huangq4@outlook.com>
2024-05-01 13:53:57 -0400

    fixing div superpositions bug

--- site/src/pages/intro/Intro.jsx
+++ site/src/pages/intro/Intro.jsx
@@ -1,7 +1,25 @@
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 import './intro.css'
 
 const Intro = () => {
+  useEffect(() => {
+    // Used to disable animation load on page load
+    const DisableLoadAnimation = () => {
+      let campaign = document.getElementById('campaign')
+      let quickstart = document.getElementById('quickstart')
+      let multiplayer = document.getElementById('multiplayer')
+      
+      const addAnimCampaign = () => {
+        console.log('campaign mouse over')
+        document.getElementById('campaign-image-1').classList.add('back-animation')
+        campaign.removeEventListener('mouseover', addAnimCampaign)
+      }
+      campaign.addEventListener('mouseover', addAnimCampaign)
+    }
+    DisableLoadAnimation()
+  })
+
   return (
     <div id="intro-container">
       <h1 id='title'>The Poker Playground</h1>
@@ -12,8 +30,17 @@ const Intro = () => {
           </button>
           <img src="../../src/assets/ui/campaign-castle.svg" alt="doesnt work" id="campaign-image-1"></img>
         </Link>
-        <Link className="react-link-clickable" to='/playground'><button id="quickstart" className='link-button'>Quick Start</button></Link>
-        <Link className="react-link-clickable" to='/host'><button id="create-table" className='link-button'>Create a Table</button></Link>
+        <Link className="react-link-clickable" to='/playground'>
+          <button id="quickstart" className='link-button'>
+            Quick Start
+          </button></Link>
+          <img src="" alt="placeholder" id="quickstart-image-1"></img>
+        <Link className="react-link-clickable" to='/host'>
+          <button id="multiplayer" className='link-button'>
+            Create a Table
+          </button>
+          <img src="" alt="wtf" id="multiplayer-image-1"></img>
+        </Link>
       </div>
     </div>
   )
