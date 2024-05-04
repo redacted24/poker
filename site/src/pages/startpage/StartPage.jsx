d0a8cf199e20a4cff16f6e1487b527ba9f32bf42
Crazydodo123 <peizheg@gmail.com>
2024-05-03 21:38:40 -0400

    add animation when page is loaded

--- site/src/pages/startpage/StartPage.jsx
+++ site/src/pages/startpage/StartPage.jsx
@@ -9,13 +9,6 @@ const StartPage = () => {
   const [username, setUsername] = useState('')
   const navigate = useNavigate()
 
-  const buttonStyle = () => {
-    if (username) {
-      return { visibility: 'visible', opacity: 1, scale: 1 }
-    } else {
-      return { visibility: 'hidden', opacity: 0 }
-    }
-  }
 
   const setName = () => {
     ls.set('username', username, { ttl: 60 * 60 })
@@ -24,11 +17,11 @@ const StartPage = () => {
 
   return (
     <div id="intro-container">
-      <h1 id='title'>The Poker Playground</h1>
+      <h1 id='prompt-title'>The Poker Playground</h1>
       <div id='prompt'>
         <label htmlFor='prompt-input' id='prompt-question'>Please enter your username: </label>
         <input id='prompt-input' type='text' value={username} onChange={(e) => setUsername(e.target.value)}></input>
-        <button id='prompt-confirm' style={buttonStyle()} onClick={setName}>Confirm</button>
+        {username && <button id='prompt-confirm' onClick={setName}>Confirm</button>}
       </div>
     </div>
   )
