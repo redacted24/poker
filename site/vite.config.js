069c2746da20a7852655f1986d8d1dc7190fb698
Crazydodo123 <peizheg@gmail.com>
2024-05-07 23:27:24 -0400

    change config file

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  server: {
-    host: true,
-    port: 443,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+  return defineConfig({
+    plugins: [react()],
+    base: "./",
+    server: {
+      host: true,
+      port: 443,
+      https: {
+        key: process.env.VITE_PRIVKEY,
+        cert: process.env.VITE_FULLCHAIN,
       },
-    }
-  },
-})
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false
+        },
+      }
+    },
+  })
+}
